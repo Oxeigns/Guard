5872d6136723e77a246b7082c224e08fbd3814ca
Samrat <yashsamrat10080@gmail.com>
2025-07-03 14:17:12 +0530

    Delete utils directory

--- utils/perms.py
+++
@@ -1,8 +0,0 @@
-"""Permission helpers."""
-
-from pyrogram.types import ChatMember
-
-
-async def is_user_admin(member: ChatMember) -> bool:
-    """Return whether the member has administrative permissions."""
-    return member.status in ("administrator", "creator")
