791b20d17705df85b7ac30acc6d39e5e42cd904b
Samrat <yashsamrat10080@gmail.com>
2025-07-04 17:35:17 +0530

    Improve startup logging

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,9 @@
 """Register all bot handlers."""
 
 from pyrogram import Client
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Explicit imports to avoid shadowing
 from . import (
@@ -15,10 +18,17 @@ from . import (
 
 
 def register_all(app: Client) -> None:
+    logger.info("Registering general handlers")
     general.register(app)
+    logger.info("Registering admin handlers")
     admin.register(app)
+    logger.info("Registering settings handlers")
     settings.register(app)
+    logger.info("Registering custom filters")
     custom_filters.register(app)
+    logger.info("Registering callback handlers")
     callbacks.register(app)
+    logger.info("Registering ping handlers")
     ping.register(app)
+    logger.info("Registering message logging handlers")
     msg_logging.register(app)
