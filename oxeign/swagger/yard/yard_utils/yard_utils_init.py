7c7044fd1f90c795c17a59d6248582bb1888103a
Samrat <yashsamrat10080@gmail.com>
2025-07-06 01:53:25 +0530

    Update yard_utils_init.py

--- oxeign/swagger/yard/yard_utils/yard_utils_init.py
+++ oxeign/swagger/yard/yard_utils/yard_utils_init.py
@@ -1,3 +1,6 @@
+# utils/__init__.py
+# Exposes key utility modules for use elsewhere in the project
+
 from . import db, errors, perms, webhook, messages
 
 __all__ = ["db", "errors", "perms", "webhook", "messages"]
